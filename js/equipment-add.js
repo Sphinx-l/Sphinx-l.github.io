60b24ec1c33fb22b8c23fb04eb979172f44fa8b8
Sphinx-l <1743995733@qq.com>
2022-11-14 17:42:39 +0800

    11-14
    改了两个bug，一个是只能点一次，另一个是新生成的点不了。原因是一样的，绑定click只能给已经有的dom元素绑定，未来的绑定要用on方法

--- js/equipment-add.js
+++ js/equipment-add.js
@@ -2,20 +2,33 @@
     var equipment2="<select><option>渴血</option><option >星蚀</option><option >狂风</option><option >神分</option><option >大饮魔刀</option><option >赛瑞尔达的怨恨</option><option >挺近</option><option >黑切</option></option><option >血手</option>"
     var equipment3="<select><option>攻速鞋</option><option >护甲鞋</option><option >法穿鞋</option><option >水银鞋</option><option >冷却鞋</option></select>"
     var equipment4="<select><option>复活甲</option><option >绿甲</option><option >夜之锋刃</option><option >中亚沙漏</option><option >水银弯刀</option></select>"
-
+    function test(value){
+      let reg=/^[+-]?(\d|([1-9]\d+))(\.\d+)?$/;
+      if(!reg.test(value))
+      alert('输入的必须是数字!');
+      else if(value>100)
+      alert('输入的数字不能大于100!');
+      else if(value<0)
+      alert('输入的数字不能小于0!');
+      else
+      return true;
+    }
+    $(".equipment").on("blur",'.onstagetext',function(){
+      if(test($(this).val()))
+      $(this).replaceWith('<span class="onstage">'+$(this).val()+'%</span>');
+    });
+    $(".equipment").on("blur",'.wintext',function(){
+      if(test($(this).val()))
+      $(this).replaceWith('<span class="win">'+$(this).val()+'%</span>');
+  
+    });
     //装备胜率、登场率
-  $(".onstage").click(function(){
-    $(this).replaceWith('<input type="text" class="onstagetext"></input>');
-  });
-  $(".equipment").on("blur",'.onstagetext',function(){
-    $(this).replaceWith('<span class="onstage">'+$(this).val()+'%</span>');
-  });
-  $(".win").click(function(){
-    $(this).replaceWith('<input type="text" class="wintext"></input>');
-  });
-  $(".equipment").on("blur",'.wintext',function(){
-    $(this).replaceWith('<span class="win">'+$(this).val()+'%</span>');
-  });
+    $(".equipment").on("click",".onstage",function(){
+      $(this).replaceWith('<input type="text" class="onstagetext"></input>');
+    })
+    $(".equipment").on("click",".win",function(){
+      $(this).replaceWith('<input type="text" class="wintext"></input>');
+    })
   //装备
   $(".equipment").on("click",'button',function(){
     $(this).parent().find('.listdetail').eq(0).clone().appendTo($(this).parent().find('.list'));
